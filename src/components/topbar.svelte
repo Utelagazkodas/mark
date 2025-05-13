4a6a2bfb4eef7d274d0494434ffbc183707f7fee
utelagazkodas <vargamiki53@gmail.com>
2025-05-13 09:40:25 +0200

    infó infó

---
+++ src/components/topbar.svelte
@@ -0,0 +1,43 @@
+<script>
+  $: outerWidth = 0;
+  $: innerWidth = 0;
+  $: outerHeight = 0;
+  $: innerHeight = 0;
+
+  import Icon from "@iconify/svelte";
+
+  import Drawer from "svelte-drawer-component";
+  let open = false;
+</script>
+
+<svelte:window
+  bind:innerWidth
+  bind:outerWidth
+  bind:innerHeight
+  bind:outerHeight
+/>
+
+<div class="w-screen h-12 flex items-center absolute">
+  {#if innerHeight < innerWidth && false}
+    gép
+  {:else}
+    <button
+    
+      on:click={() => {
+        open = true;
+      }}
+      ><Icon
+        icon="material-symbols:menu-rounded"
+        width="32"
+        height="32"
+      /></button
+    >
+    <div class="flex-1 text-center">
+        Márk Károlyi hair
+    </div>
+  {/if}
+</div>
+
+<Drawer {open} size="75vw" on:clickAway={()=>{open=false}}>
+  <button on:click={() => (open = false)}>Close</button>
+</Drawer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-	
+	import Topbar from '../components/topbar.svelte';
 	import '../app.css';
 	
 	let { children } = $props();
 </script>
 
+<Topbar/>
+
+<div class="overflow-y-scroll overflow-x-hidden">
 {@render children()}
+</div>
