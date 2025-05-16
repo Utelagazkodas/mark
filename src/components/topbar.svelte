c12711456f55cd3f48610476e9fab7b9fcdb9567
utelagazkodas <vargamiki53@gmail.com>
2025-05-16 18:57:48 +0200

    why the hell would anyone even read this?

--- src/components/topbar.svelte
+++ src/components/topbar.svelte
@@ -6,27 +6,27 @@
 
   import Drawer from "./Drawer.svelte";
   import { isMobile, scroll } from "$lib";
+  import { onMount } from "svelte";
   let open = false;
 
-  let TopBarComponent : HTMLDivElement
-  
-  scroll.subscribe((value)=>{
-    TopBarComponent.style.opacity = "0"
-  })
+  let TopBarComponent: HTMLDivElement;
 
+  onMount(() => {
+    scroll.subscribe((value) => {
+      TopBarComponent.style.opacity = `${value / 30}`;
+
+      TopBarComponent.style.zIndex = value == 0 ? "-100" : "10"
+    });
+  });
 </script>
 
-<svelte:window
-  bind:innerWidth
-  bind:innerHeight
-/>
+<svelte:window bind:innerWidth bind:innerHeight />
 
 <div
   bind:this={TopBarComponent}
-
-  class="w-screen h-12 flex items-center absolute top-0 backdrop-blur-xs border-b bg-white/60"
+  class="w-screen h-12 flex items-center absolute top-0 backdrop-blur-xs border-b bg-white/60 transition-all duration-200 opacity-0"
 >
-  {#if !isMobile(innerWidth ,innerHeight)}
+  {#if !isMobile(innerWidth, innerHeight)}
     gép
   {:else}
     <!--IGNORE THIS NOT, MY FINEST WORK, this is so that the text is in the middle-->
@@ -35,11 +35,13 @@
     <div class="flex-1 text-center">Márk Károlyi hair</div>
 
     <button
-      class="mr-3 hover:cursor-pointer"
+      class={`mr-3 clickable-on-scroll ${$scroll > 0 ? "hover:cursor-pointer" : ""}`}
+
       on:click={() => {
         open = true;
       }}
       ><Icon
+      
         icon="material-symbols:menu-rounded"
         width="32px"
         height="32px"
@@ -55,13 +57,16 @@
     open = false;
   }}
   placement={"right"}
-  
+  duration={0.2}
 >
-  
-    <div class="h-screen w-full bg-white/35 backdrop-blur-xs relative">
-        <button class="absolute top-3 right-3 hover:cursor-pointer" on:click={()=>{open=false}}>
-          <Icon icon="material-symbols:close-rounded" width="32" height="32" />
-        </button>
-    </div>
-  
+  <div class="h-screen w-full bg-white/35 backdrop-blur-xs relative">
+    <button
+      class="absolute top-3 right-3 hover:cursor-pointer"
+      on:click={() => {
+        open = false;
+      }}
+    >
+      <Icon icon="material-symbols:close-rounded" width="32" height="32" />
+    </button>
+  </div>
 </Drawer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
-{#each {length: 100} as item}
-     <div id={"d"+item}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum dolorum similique voluptatem, neque quo deleniti quidem asperiores. Quidem ea veritatis minima voluptate maiores? Quam dolorum praesentium consequatur dignissimos ex ab.
-     </div>
-{/each}
+<div class="h-screen relative">
+  <div>
+    <div>Márk Károlyi</div>
+    <div>Hair Stylist</div>
+  </div>
+
+  <div class="absolute">
+
+  </div>
+</div>
